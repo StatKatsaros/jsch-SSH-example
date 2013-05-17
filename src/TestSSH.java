cd85bc569e91d9dc8e07f4c0e20de46920f6c85b
Frank Hanner <luapdeux@gmail.com>
2013-05-16 17:32:18 -0700

    Code push

---
+++ src/TestSSH.java
@@ -0,0 +1,29 @@
+public class TestSSH {
+    VaderSSHManager instance;
+
+    public static void main(String[] args) {
+        //command to be sent to remote machine
+        String command = "ls -la";
+        //SSH Instance... using generic user/password/server name
+        instance = new VaderSSHManager("username", "password", "server", "");
+        //connect to server
+        String errorMessage = instance.connect();
+        if(errorMessage != null){
+            System.out.println("ERROR >" + errorMessage);
+        }
+        
+        //send command to server
+        String result = instance.sendCommand(command);
+        
+        //close connection
+        instance.close();
+        
+        //check what's returned... in this case .bash_history
+        if (result.contains(".bash_history")){
+            System.out.println("TRUE");
+        } else {
+            System.out.println("FALSE");
+        }
+            
+    }
+}
